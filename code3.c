35ee4ee191a055b368998e6a5880afb85d71b756
tirthasadhu015-dot <tirthasadhu015@gmail.com>
2026-09-07 11:10:19 +0530

    code 3-write a array program in C to find a sorted array.

---
+++ code3.c
@@ -0,0 +1,28 @@
+//write a array program in C to find a sorted array.
+#include <stdio.h>
+int main() {
+    int n, i, j, temp;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j + 1]
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    printf("The sorted array is: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
